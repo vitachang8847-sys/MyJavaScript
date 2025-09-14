91a12e7cbd321761dd8172a034adc52db2f3f6db
vitachang <vitachang8847@gmail.com>
2025-09-14 16:36:23 +0800

    20250914

---
+++ 20250914/plugins/2_loki.js
@@ -0,0 +1,12 @@
+
+        //alert(彈跳視窗)
+    console.log(123);
+    console.log("hello world，你好世界!")
+
+    /* 多行註解
+    console.log(123);
+    console.log("hello world，你好世界!")*/
+
+    //RD 測試指令
+    console.warn("hello world，你好世界!")
+    console.error("hello world，你好世界!")
