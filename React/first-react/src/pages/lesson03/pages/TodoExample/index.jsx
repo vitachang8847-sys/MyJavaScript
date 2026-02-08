86ffcb2dfb0df38f9061685cbb8fcd6adc4464ba
vitachang <vitachang8847@gmail.com>
2026-02-08 14:44:40 +0800

    20260208_PM6 優化 reducer 分割權責檔案

--- React/first-react/src/pages/lesson03/pages/TodoExample/index.jsx
+++ React/first-react/src/pages/lesson03/pages/TodoExample/index.jsx
@@ -1,49 +1,24 @@
+import { addTodo, toggleChecked, deleteTodo } from './store/action';
 import TaskAdd from './TaskAdd';
 import TaskList from './TaskList';
 import { useReducer } from 'react';
-
-const ADD_TODO = 'ADD';
-const DELETE_TODO = 'DELETE';
-const TOGGLE_CHECKED = 'TOGGLE_CHECKED';
-
-
-// 🌟 初始資料
-const initState = [
-  { id: 1, text: '去健身房', checked: false },
-  { id: 2, text: '繳帳單', checked: true },
-  { id: 3, text: '見 George', checked: false },
-  { id: 4, text: '買雞蛋', checked: false },
-  { id: 5, text: '讀一本書', checked: false },
-];
-
-function todoReducer(state, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { id: state.length ? state[state.length - 1].id + 1 : 1, text: action.text, checked: false }];
-    case DELETE_TODO:
-      return state.filter((item) => item.id !== action.id);
-    case TOGGLE_CHECKED:
-      return state.map((item) => (item.id === action.id ? { ...item, checked: !item.checked } : item));
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
-}
+import { initState, todoReducer } from './store/reducer';
 
 export default function TodoExample() {
-  const [todoList, dispatch] = useReducer(todoReducer, initState); // 先放著，後面會介紹 useReducer
+  const [todoList, dispatch] = useReducer(todoReducer, initState);
 
   return (
     <div className="todo-example">
       <h1>Todo List：useReducer 範例</h1>
 
       {/* 輸入區域 */}
-      <TaskAdd onAdd={(text) => dispatch({ type: 'ADD', text })} />
+      <TaskAdd onAdd={(text) => dispatch(addTodo(text))} />
 
       {/* 待辦列表 */}
       <TaskList
         todoList={todoList}
-        onDelete={(id) => dispatch({ type: 'DELETE', id })}
-        onToggleChecked={(id) => dispatch({ type: 'TOGGLE_CHECKED', id })}
+        onDelete={(id) => dispatch(deleteTodo(id))}
+        onToggleChecked={(id) => dispatch(toggleChecked(id))}
       />
     </div>
   );
